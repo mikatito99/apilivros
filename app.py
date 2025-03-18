61868f7d7cd2d26a730bc40f6bc25e40fb310833
mikaela <mikaelatito6@gmail.com>
2025-03-18 20:11:58 -0300

    Correção aplicada

--- app.py
+++ app.py
@@ -47,29 +47,17 @@ def doar():
 
         return jsonify({"mensagem": "Livro cadastrado com sucesso"}), 201
 
-# Rota para listar todos os livros cadastrados
 
-
-# Agora vamos criar a rota que irá listar os livros cadastrados, ou seja a rota que irá receber o GET (requisição)
 @app.route("/livros", methods=["GET"])
 def listar_livros():
     """
     Retorna todos os livros cadastrados no banco de dados.
     """
 
-    # Como vamos puxar os livros que foram cadastrados no banco de dados, devemos refazer mais uma vez a conexão com o banco de dados
     with sqlite3.connect("database.db") as conn:
-        # Esse comando é responsável por buscar todos os livros armazenados no banco de dados
+
         livros = conn.execute("SELECT * FROM livros").fetchall()
 
-        # Vamos decompor e entender esses comandos
-        # conn.execute --> Como sabemos todo e qualquer comando que executamos no banco de dados precisamos do conn, ele é a ponte entre o banco de dados e o código Python
-        # SELECT * FROM LIVROS --> Consulte todas as informações da tabela Livros
-        # fetchall() --> É utilizado para buscar todos os dados retornados por uma consulta SQL e retorna-lós em formato de tupla
-
-        # Por mais que utilizamos o SELECT * FROM LIVROS, esse comando fará somente a consulta dos dados, para que realmente consigamos pegar esses dados formatados precisamos do fetchall() que os pegará e transformará eles em formato de tupla
-
-    # Criamos esse array, pois ele será responsável por armazenar todos os valores que puxarmos do nosso banco de dados
     livros_formatados = []
 
     for livro in livros:
